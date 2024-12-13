bed4f0db143a61e14ef1cc80ee1b8547faf8d8ee
tab Jesus <jcampoy@unicartagena.edu.co>
2024-12-13 01:17:52 -0500

    Second commit

--- s2/Main/Usuario.java
+++ s2/Main/Usuario.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package co.edu.udec.s2.Main;
 
 /**
  *
  * @author JESUS DAVID
+ * Usuario (Nombre del usuario, 
+ * Número de teléfono del usuario,
+ * Correo del usuario) 
  */
 public class Usuario {
     
+    String nombre;
+    String telefono;
+    String email;
+    
+    public Usuario (String nombre, String telefono, String email){
+        
+       this.email = email;
+       this.nombre = nombre;
+       this.telefono = telefono;
+    }
+    
+    
+    public String getTelefono (){
+        
+        return telefono;
+        
+    }
+   
+    public boolean validarTelefono (){
+        
+        return this.telefono.length() == 10;
+    }
+    
+    
+    public boolean validarEmail(){
+        
+        return this.email.contains("@");
+    }
+
 }
