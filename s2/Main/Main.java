065153aab7dc9f67e0dcb3343f83cd3d64b1a0cd
tab Jesus <jcampoy@unicartagena.edu.co>
2024-12-13 01:14:24 -0500

    PRIMER COMMIT

---
+++ s2/Main/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.udec.s2.Main;
+
+/**
+ *
+ * @author JESUS DAVID
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JESUS");
+    }
+}
---
+++ s2/Main/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.udec.s2.Main;
+
+/**
+ *
+ * @author JESUS DAVID
+ */
+public class Usuario {
+    
+}
